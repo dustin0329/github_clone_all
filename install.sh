97a01c7f07ddb01777e50fa1a376fa2c7eb5fa16
dustin0329 <dustin0329@gmail.com>
2018-10-12 22:11:14 +0800

    temp remove git install

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 #git clone vimrc
-cd
-git clone https://github.com/dustin0329/vimrc.git ~/.vim
-cd ~/.vim
-git submodule update --init
-
-./install-vimrc.sh
-
-cd .vim/bundle/command-t/ruby/command-t
-ruby extconf.rb
-make
+#cd
+#git clone https://github.com/dustin0329/vimrc.git ~/.vim
+#cd ~/.vim
+#git submodule update --init
+#
+#./install-vimrc.sh
+#
+#cd .vim/bundle/command-t/ruby/command-t
+#ruby extconf.rb
+#make
 
 
 #git clone profile
